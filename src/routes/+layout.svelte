74da4482c38cbb26c75a6e9d131643c3f94b7545
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-04-23 20:48:46 +0530

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
 	import '../app.css';
 	
 	let { children } = $props();
@@ -6,6 +6,17 @@
 
 <div class="relative mx-auto min-h-screen max-w-2xl bg-background px-6 py-12 font-sans antialiased sm:py-24">
 	{@render children()}
+</div> -->
+
+<script>
+	import Navbar from '$lib/components/Portfolio/Navbar.svelte';
+	import '../app.css';
+	import { ModeWatcher, setMode } from 'mode-watcher';
+	setMode('dark');
+</script>
+
+<ModeWatcher />
+<div class="relative mx-auto min-h-screen max-w-2xl bg-background px-6 py-12 font-sans antialiased sm:py-24">
+	<slot></slot>
+	<Navbar />
 </div>
-
-
