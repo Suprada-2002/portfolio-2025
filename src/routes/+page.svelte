e3243c1c555b2021eb3fac8d729f7820ebddaaf2
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-04-24 19:32:11 +0530

    changing home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,9 @@
     import { DATA } from '$lib/data/resume';
     import { marked } from 'marked';
     let BLUR_FADE_DELAY = 0.04;
+
+	import WorkCard from "$lib/components/Design/WorkCard.svelte";
+	import ProjectCard from "$lib/components/Portfolio/ProjectCard.svelte";
 </script>
 
 <main class="flex min-h-[100dvh] flex-col space-y-10">
@@ -33,33 +36,53 @@
 		</div>
 	</section>
 
-	<section id="about">
-			<BlurFade delay={BLUR_FADE_DELAY}>
-				<h2 class="text-xl font-bold">About</h2>
-			</BlurFade>
-			<BlurFade delay={BLUR_FADE_DELAY * 1.4}>
-				<div
-					class="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert"
-				>
-					{@html marked(DATA.summary)}
-				</div>
-			</BlurFade>
+    <section id="about">
+		<BlurFade delay={BLUR_FADE_DELAY}>
+			<h2 class="text-xl font-bold">About</h2>
+		</BlurFade>
+		<BlurFade delay={BLUR_FADE_DELAY * 1.4}>
+			<div
+				class="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert"
+			>
+				{@html marked(DATA.summary)}
+			</div>
+		</BlurFade>
 	</section>
 
-    <section id="work">
+    <section id="education">
 		<div class="flex min-h-0 flex-col gap-y-3">
 			<BlurFade delay={BLUR_FADE_DELAY}>
-				<h2 class="text-xl font-bold">Work Experience</h2>
+				<h2 class="text-xl font-bold">Education</h2>
 			</BlurFade>
-			{#each DATA.work as work, id}
+			{#each DATA.education as edu, id}
 				<BlurFade delay={BLUR_FADE_DELAY * 1.2 + id * 0.05}>
-					<ResumeCard {...work} />
+					<ResumeCard {...edu} />
 				</BlurFade>
 			{/each}
 		</div>
 	</section>
 
-	<section id="education">
+
+	<section id="work">
+		<div class="flex min-h-0 flex-col gap-y-3">
+			<BlurFade delay={BLUR_FADE_DELAY}>
+				<h2 class="text-xl font-bold">Work Experience</h2>
+			</BlurFade>
+			<BlurFade delay={BLUR_FADE_DELAY * 2}>
+				<ul class="mb-4 ml-4 divide-y divide-dashed border-l">
+					{#each DATA.workProfile as project}
+						<BlurFade delay={BLUR_FADE_DELAY}>
+							<WorkCard {...project} />
+						</BlurFade>
+					{/each}
+				</ul>
+			</BlurFade>
+		</div>
+	</section>
+
+
+
+    <!-- <section id="education">
 		<div class="flex min-h-0 flex-col gap-y-3">
 			<BlurFade delay={BLUR_FADE_DELAY}>
 				<h2 class="text-xl font-bold">Education</h2>
@@ -78,7 +101,48 @@
 				</BlurFade>
 			{/each}
 		</div>
+	</section> -->
+
+
+	<section id="projects">
+		<div class="w-full space-y-12 py-12">
+			<BlurFade delay={BLUR_FADE_DELAY}>
+				<div class="flex flex-col items-center justify-center space-y-4 text-center">
+					<div class="space-y-2">
+						<div class="inline-block rounded-lg bg-foreground px-3 py-1 text-sm text-background">
+							My Projects
+						</div>
+						<h2 class="text-3xl font-bold tracking-tighter sm:text-5xl">
+							Check out my latest work
+						</h2>
+						<p
+							class="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed"
+						>
+							I&apos;ve worked on a variety of projects, from simple websites to complex web
+							applications. Here are a few of my favorites.
+						</p>
+					</div>
+				</div>
+			</BlurFade>
+			<div class="mx-auto grid max-w-[800px] grid-cols-1 gap-3 sm:grid-cols-2">
+				{#each DATA.projects as project, id}
+					<BlurFade delay={BLUR_FADE_DELAY * 1.5 + id * 0.05}>
+						<ProjectCard
+							href={project.href}
+							title={project.title}
+							description={project.description}
+							dates={project.dates}
+							tags={project.technologies}
+							image={project.image}
+							video={project.video}
+							links={project.links}
+						/>
+					</BlurFade>
+				{/each}
+			</div>
+		</div>
 	</section>
+
 
     <section id="skills">
 		<div class="flex min-h-0 flex-col gap-y-3">
@@ -95,6 +159,7 @@
 		</div>
 	</section>
 
+
     <section id="contact">
 		<div class="grid w-full items-center justify-center gap-4 px-4 py-12 text-center md:px-6">
 			<BlurFade delay={BLUR_FADE_DELAY * 2}>
@@ -106,15 +171,19 @@
 					<p
 						class="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed"
 					>
-						Want to chat? Just shoot me a dm
-						<a href="http:/www.google.com" class="text-blue-500 hover:underline">
-							with a direct question on twitter
+						Want to connect? Just shoot me a dm or
+						<a href="https://guestbook-eta.vercel.app/" class="text-blue-500 hover:underline">
+							sign my guestboook
 						</a>
-						and I&apos;ll respond whenever I can. I will ignore all soliciting.
+						and I&apos;ll respond as soon as I can.
 					</p>
 				</div>
+				<div
+				class="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert" >
+				Made with ❤️ and Svelte
+			</div>
 			</BlurFade>
 		</div>
 	</section>
-    
+
 </main>
